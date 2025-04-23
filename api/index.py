cda0e8d67d45e540be3a80c6e1b5bf295d1401a8
Ishatva Singh Panwar <115392210+ispanwar@users.noreply.github.com>
2025-04-23 19:44:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import joblib
+import numpy as np
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
+CORS(app)
+
+model = joblib.load('iris_model.pkl')
+
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Welcome to the iris predictor'
 
-@app.route('/about')
-def about():
-    return 'About'
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = request.get_json()
+    features = data['features']
+    features = np.array(features).reshape(1,-1)
+    prediction = model.predict(features)
+    return jsonify({"prediction":int(prediction[0])})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
